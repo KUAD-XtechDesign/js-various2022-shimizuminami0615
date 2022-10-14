1c6b74fa048d31736fdf0757142f7e41b1c16df1
shimizuminami0615 <m-shimizu7278@st.kyoto-art.ac.jp>
2022-10-14 18:07:48 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -3,7 +3,12 @@ $(function(){
   $("#intro p").hide().fadeIn(1000)
   //setTimeout(introAnime,2000);//2秒経った時にintroAnimeを実行
   $("#intro").on("click",introAnime)//クリックしたらintroAnimeを実行
-
+  
+  
+  
+  
+ 
+ 
   function introAnime(){//jqueryでフェードアウト、終わったらcontentAnime実行
     $("#intro").fadeOut(1000,contentAnime);
   }
